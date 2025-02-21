dbc56d8223b5bc89d3efa97b9a00815595ec57f4
Oscaner Miao <oscaner1997@gmail.com>
2025-02-21 14:14:10 +0800

    feat: silent

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,10 +1,10 @@
 autoload -U add-zsh-hook
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
-    nvm use
+    nvm use --silent
   elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    echo "Reverting to nvm default version"
-    nvm use default
+    # echo "Reverting to nvm default version"
+    nvm use default --silent
   fi
 }
 add-zsh-hook chpwd load-nvmrc
