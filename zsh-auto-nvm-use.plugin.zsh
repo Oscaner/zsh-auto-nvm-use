9ffd26f9d1c8f707d6e4036dc28d78d0c86ff007
Oscaner Miao <oscaner1997@gmail.com>
2025-06-21 10:55:23 +0800

    remove debug

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,18 +1,18 @@
 autoload -U add-zsh-hook
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
-    echo "Loading nvm version from .nvmrc"
+    # echo "Loading nvm version from .nvmrc"
 
     # Check if nvm is installed
     local nvm_version=$(nvm version "$(cat .nvmrc)")
     if echo $nvm_version | grep -q "N/A"; then
-      echo "nvm version from .nvmrc not found, installing..."
+      # echo "nvm version from .nvmrc not found, installing..."
       nvm install
     fi
 
     nvm use --silent
   elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    echo "Reverting to nvm default version"
+    # echo "Reverting to nvm default version"
     nvm use default --silent
   fi
 }
