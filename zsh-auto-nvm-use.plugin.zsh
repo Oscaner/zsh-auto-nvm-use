f940873722f68270f41edd5fd5738f46325230a4
Oscaner Miao <oscaner1997@gmail.com>
2025-06-21 10:41:17 +0800

    feat: install if not exists

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,6 +1,14 @@
 autoload -U add-zsh-hook
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
+    # echo "Loading nvm version from .nvmrc"
+
+    # Check if nvm is installed
+    local nvm_version=$(nvm version "$(cat .nvmrc)")
+    if echo $nvm_version | grep -q "N/A"; then
+      nvm install --no-progress
+    fi
+
     nvm use --silent
   elif [[ $(nvm version) != $(nvm version default)  ]]; then
     # echo "Reverting to nvm default version"
